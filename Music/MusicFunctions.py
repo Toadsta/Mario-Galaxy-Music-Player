d81468e5ed41f0d9064551d0e6470e6f703c2331
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-11-19 22:09:59 +0000

    add - added items in the main file to a main function and reloaded get item code

--- Music/MusicFunctions.py
+++ Music/MusicFunctions.py
@@ -23,6 +23,21 @@ def unpause():
     pg.mixer.music.unpause()
 
 
+def get_element(event):
+    selection = event.widget.curselection()
+    index = selection[0]
+    value = event.widget.get(index)
+    if value == "Overtune":
+        overtune()
+        play()
+    elif value == "Gusty Garden Galaxy":
+        gusty_garden_galaxy()
+        play()
+    else:
+        print("How?!")
+        # adapted from https://www.code4example.com/python/python3-get-selected-value-from-listbox-in-tkinter/
+
+
 def skip():
     pass
 
--- main.py
+++ main.py
@@ -3,60 +3,51 @@ from tkinter import *
 from Music import MusicFunctions as Mf
 import pygame as pg
 
-pg.mixer.init()
 
-def get_element(event):
-    selection = event.widget.curselection()
-    index = selection[0]
-    value = event.widget.get(index)
-    if value == "Overtune":
-        Mf.overtune()
-        Mf.play()
-    elif value == "Gusty Garden Galaxy":
-        Mf.gusty_garden_galaxy()
-        Mf.play()
-    else:
-        print("How?!")
+def main():
+    pg.mixer.init()
+
+    # creates a window
+    window = Tk()
+    window.title("Music Player")
+    window.geometry("250x300")
+    window.resizable(False, False)
+    # creates a label
+    # title = Label(window, text="Mario Galaxy: Music Player")
+
+    # add to window
+    # title.pack()
+
+    # create a quit box
+    # tkinter.Button(window, text="Quit", command=window.destroy).pack()
+
+    # creates a list box
+    list_box = Listbox(window)
+
+    list_box.insert(0, "Overtune")
+    list_box.insert(1, "Gusty Garden Galaxy")
+
+    # create an play music button
+    # play_button = Button(window, text="Play", command=play)
+    # play_button.pack(side='bottom')
+
+    # play_selected = Button(window, text="Play", command=selected_item)
+    # play_selected.pack(side='bottom')
+
+    pause = Button(window, text="Pause", command=Mf.pause)
+    pause.place(x=10, y=230)
+
+    unpause = Button(window, text="Unpause", command=Mf.unpause)
+    unpause.place(x=150, y=230)
+
+    list_box.bind('<<ListboxSelect>>', Mf.get_element)
+
+    # add to window
+    list_box.pack()
+
+    # waits for user inout
+    window.mainloop()
 
 
-# creates a window
-window = Tk()
-window.title("Music Player")
-window.geometry("250x300")
-window.resizable(False, False)
-# creates a label
-# title = Label(window, text="Mario Galaxy: Music Player")
-
-# add to window
-# title.pack()
-
-# create a quit box
-# tkinter.Button(window, text="Quit", command=window.destroy).pack()
-
-# creates a list box
-list_box = Listbox(window)
-
-list_box.insert(0, "Overtune")
-list_box.insert(1, "Gusty Garden Galaxy")
-
-
-# create an play music button
-# play_button = Button(window, text="Play", command=play)
-# play_button.pack(side='bottom')
-
-# play_selected = Button(window, text="Play", command=selected_item)
-# play_selected.pack(side='bottom')
-
-pause = Button(window, text="Pause", command=Mf.pause)
-pause.place(x=10, y=230)
-
-unpause = Button(window, text="Unpause", command=Mf.unpause)
-unpause.place(x=150, y=230)
-
-list_box.bind('<<ListboxSelect>>', get_element)
-
-# add to window
-list_box.pack()
-
-# waits for user inout
-window.mainloop()
+if __name__ == '__main__':
+    main()
